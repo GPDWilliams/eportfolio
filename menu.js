9a9ad1a8437789eca725b777a39fafe3675237eb
GPDWilliams <75958021+GPDWilliams@users.noreply.github.com>
2024-02-15 23:16:55 +0000

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,6 +3,10 @@
 
 var origY = 0; 
 
+function sbar() {
+window.status = ''; 
+}
+
 function showMenu() {
 //debugger;
 origY = document.getElementById("menubox").offsetTop;
@@ -21,4 +25,30 @@ function hideMenu() {
 document.getElementById("menubox").style.top = origY;
 document.getElementById("menubox").style.zIndex = "-1";
 }
+
+function goto(x, y) {
+
+window.scrollTo({
+  top: y,
+  left: x,
+  behavior: "smooth",
+});
+
+}
+
+function goto2(element) {
+height_top=95;
+//height_element=document.getElementById(element).offsetHeight;
+x=0;
+y=document.getElementById(element).offsetTop - height_top;
+
+window.scrollTo({
+  top: y,
+  left: x,
+  behavior: "smooth",
+});
+
+}
+
+
 
